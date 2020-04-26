9c6f29dbb7d9e40be70a058797d83f9b2411be19
Justin Dennison <justin1dennison@gmail.com>
2020-04-26 16:45:56 -0400

    added tests related list of numbers

--- tests/test_pipeliner.py
+++ tests/test_pipeliner.py
@@ -1,11 +1,11 @@
 from pipeliner import __version__, pipeliner
 
 
-def _square(x):
+def _square(x: int) -> int:
     return x * x
 
 
-def _inc(x):
+def _inc(x: int) -> int:
     return x + 1
 
 
@@ -27,3 +27,20 @@ def test_pipeliner_with_other_operators():
     result = square(2) | inc | square
 
     assert result == 25
+
+def test_pipeliner_with_strings():
+    @pipeliner(">>")
+    def double(xs: list):
+        return (2 * x for x in xs)
+
+    @pipeliner(">>")
+    def inc(xs: list):
+        return (x + 1 for x in xs)
+
+    @pipeliner(">>")
+    def collapse(xs: list):
+        return sum(xs)
+
+    result = [1, 2, 3] >> inc >> double >> collapse
+
+    assert result == 18
